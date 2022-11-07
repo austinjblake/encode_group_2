c45144a8eea941103ecde2ed8e7f45a2b6a8dbf6
Austin Blake <ajblake2189@gmail.com>
2022-11-06 21:37:43 -0600

    deployments clean up

--- scripts/Deployments.ts
+++ scripts/Deployments.ts
@@ -14,30 +14,24 @@ function convertStringArrayToBytes32(array: string[]) {
 }
 
 export async function getSigner() {
-  let signer;
-  let env;
   const provider = ethers.getDefaultProvider("goerli", {
     alchemy: process.env.ALCHEMY_API_KEY,
   });
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "");
-  signer = wallet.connect(provider);
-  env = "Goerli Testnet";
+  const signer = wallet.connect(provider);
   const balance = await signer.getBalance();
   console.log(
-    `Working on ${env} connected to wallet ${signer.address} with balance of ${balance}`
+    `Working on Goerli Testnet connected to wallet ${signer.address} with balance of ${balance}`
   );
   return signer as Wallet;
 }
 
 export async function getLocalSigner(accNum = 0) {
-  let signer;
-  let env;
   const accounts = await ethers.getSigners();
-  signer = accounts[accNum];
-  env = "Local Blockchain";
+  const signer = accounts[accNum];
   const balance = await signer.getBalance();
   console.log(
-    `Working on ${env} connected to wallet ${signer.address} with balance of ${balance}`
+    `Working on Local Blockchain connected to wallet ${signer.address} with balance of ${balance}`
   );
   return signer as unknown as Wallet;
 }
